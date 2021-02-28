3e29ddf86f87b110240598a81c9e5914717dc507
Rudo Musarurwa <Rudomusarurwa@gmail.com>
2021-02-28 12:19:07 +0000

    commit on ClassExercises

---
+++ Class Exercises/script.js
@@ -0,0 +1,16 @@
+function fade(){
+    var element = document.getElementById('circle');
+    var circleOpacity = parseFloat(element.style.opacity);
+    element.style.opacity = circleOpacity - 0.01;  
+ }
+ function timer(){
+     setInterval(fade, 100);
+ }
+ 
+ function myLoadEvent() {
+   var element = document.getElementById('circle');
+   element.addEventListener('click', timer);
+   element.style.opacity = 1;
+ }
+ 
+ document.addEventListener('DOMContentLoaded', myLoadEvent);
