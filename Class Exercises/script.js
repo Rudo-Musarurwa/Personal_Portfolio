4101e45ac41a23cb67b6834fc929a43b96186979
Rudo Musarurwa <Rudomusarurwa@gmail.com>
2021-03-02 21:14:44 +0000

    circle movement on screen

--- Class Exercises/script.js
+++ Class Exercises/script.js
@@ -1,14 +1,40 @@
-function myKeyDown(){
+function moveLeft(){
     var element = document.getElementById('circle');
-    var moveLeft = element.offsetLeft;
-    element.style.left = moveLeft- 10 +'px';
+    var leftMove = element.offsetLeft;
+    element.style.left = leftMove - 1 + 'px';
+}
+function moveRight(){
+    var element = document.getElementById('circle');
+    var rightMove = element.offsetLeft;
+    element.style.left = rightMove + 1 + 'px';
+}
+function moveDown(){
+    var element = document.getElementById('circle');
+    var downMove = element.offsetTop;
+    element.style.top = downMove + 1 + 'px';   
+}
+function moveUp(){
+    var element = document.getElementById('circle');
+    var upMove = element.offsetTop;
+    element.style.top = upMove - 1 + 'px';
+}
+
+function myKeyDown(event){
+    if (event.keyCode == 37){
+       setInterval(moveLeft, 100); 
+    }
+    if (event.keyCode == 39){
+        setInterval(moveRight, 100);
+    }
+    if (event.keyCode == 38){
+        setInterval(moveUp, 100);  
+    }
+    if (event.keyCode == 40){
+        setInterval(moveDown, 100);
+    }
  }
-/* function timer(){
-     setInterval(fade, 50);
- }*/
- 
- /*function myLoadEvent() {
-   document.addEventListener('keydown', myKeyDown);
- }*/
- document.addEventListener('keydown', myKeyDown);
- //document.addEventListener('DOMContentLoaded', myLoadEvent);
+
+ function myLoadEvent(){
+     document.addEventListener('keydown', myKeyDown);
+ }
+document.addEventListener('DOMContentLoaded', myLoadEvent);
