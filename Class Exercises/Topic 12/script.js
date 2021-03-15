5fdf4fc308b39b4fd9910d276b0ccbf0a54c82fa
Rudo Musarurwa <Rudomusarurwa@gmail.com>
2021-03-15 14:34:13 +0000

    Key release events

--- Class Exercises/Topic 12/script.js
+++ Class Exercises/Topic 12/script.js
@@ -1,68 +1,67 @@
-function moveLeft(){
-    var element = document.getElementById('circle');
-    var leftMove = element.offsetLeft;
-    element.style.left = leftMove - 1 + 'px';
-
-    if(leftMove == 0){
-        clearInterval(interval);
-        interval = setInterval(moveRight, 10);
+function moveInterval(){
+    if(leftPressed == true){
+        var circle = document.getElementById('circle');
+        var moveleft = parseFloat(circle.offsetLeft);
+        circle.style.left = moveleft - 1 +'px';
     }
-}
-function moveRight(){
-    var element = document.getElementById('circle');
-    var rightMove = element.offsetLeft;
-    element.style.left = rightMove + 1 + 'px';
-
-    if(element.offsetLeft+element.offsetWidth == window.innerWidth){
-        clearInterval(interval);
-        interval = setInterval(moveLeft, 10);
+    if(rightPressed == true){
+        var circle = document.getElementById('circle');
+        var moveRight = parseFloat(circle.offsetLeft);
+        circle.style.left = moveRight + 1 +'px';
     }
-}
-
-function moveDown(){
-    var element = document.getElementById('circle');
-    var downMove = element.offsetTop;
-    element.style.top = downMove + 1 + 'px';   
-
-    if(element.offsetTop+element.offsetHeight == window.innerHeight){
-        clearInterval(interval);
-        interval = setInterval(moveUp, 10);
+    if(upPressed == true){
+        var circle = document.getElementById('circle');
+        var moveUp = parseFloat(circle.offsetTop);
+        circle.style.top = moveUp - 1 +'px';
+    }
+    if(downPressed == true){
+        var circle = document.getElementById('circle');
+        var moveDown = parseFloat(circle.offsetTop);
+        circle.style.top = moveDown + 1 +'px';
     }
 }
 
-function moveUp(){
-    var element = document.getElementById('circle');
-    var upMove = element.offsetTop;
-    element.style.top = upMove - 1 + 'px';
-
-    if(upMove == 0){
-        clearInterval(interval);
-        interval = setInterval(moveDown, 10);
-    }
-}
-
-var interval = 0;
-var t = window.innerHeight;//top
-var l = window.innerWidth;//left
+var leftPressed = false;
+var rightPressed = false;
+var upPressed = false;
+var downPressed = false;
+var interval = 10
 
 function myKeyDown(event){
-        clearInterval(interval);
         if (event.keyCode == 37){
-        interval = setInterval(moveLeft, 10); 
+            leftPressed = true; 
         }
         if (event.keyCode == 39){
-            interval = setInterval(moveRight, 10);
+            rightPressed = true;
         }
         if (event.keyCode == 38){
-            interval = setInterval(moveUp, 10);  
+            upPressed = true;  
         }
         if (event.keyCode == 40){
-            interval = setInterval(moveDown, 10);
-        }
+            downPressed = true;
+        }  
  }
+
+ function myKeyUp(event){
+    if (event.keyCode == 37){
+        leftPressed = false; 
+    }
+    if (event.keyCode == 39){
+        rightPressed = false;
+    }
+    if (event.keyCode == 38){
+        upPressed = false;  
+    }
+    if (event.keyCode == 40){
+        downPressed = false;
+    }
+}
 
  function myLoadEvent(){
     
      document.addEventListener('keydown', myKeyDown);
+     document.addEventListener('keyup', myKeyUp);
+     setInterval(moveInterval, interval);
+
  }
 document.addEventListener('DOMContentLoaded', myLoadEvent);
